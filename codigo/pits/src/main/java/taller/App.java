f450e0f61e2d869a54a7a54e382693c4c6b3fa1e
Pablo RZ <paporuiz08@gmail.com>
2022-12-08 10:20:20 -0600

    Pantalla inicio

---
+++ codigo/pits/src/main/java/taller/App.java
@@ -0,0 +1,20 @@
+package taller;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.xml.validation.Validator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hola");
+    }
+
+    
+}
