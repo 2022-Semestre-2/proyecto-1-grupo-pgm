898e6bc544efa228fb1a69605205a0c5fb5fa2a6
Gerny Mcdiell Díaz Hall <99917975+Gerny-Diaz-Hall@users.noreply.github.com>
2022-12-20 19:37:27 -0600

    Add files via upload

---
+++ codigo/pits/src/main/java/taller/Ventana.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ventana;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ventana {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
